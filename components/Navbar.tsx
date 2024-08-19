e51bfe3ffae866836d9ecb6f91d0d5823999ffb2
apsdeveloper461 <mehboobalam786461@gmail.com>
2024-08-19 06:52:14 +0500

    setup error in building the project

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -50,7 +50,7 @@ const Navbar = () => {
 
             {/* navbar for destop and laptops */}
             <div className='hidden md:flex  items-center justify-between  h-20 px-8 lg:px-16'>
-                <div className='text-slate-300 text-2xl font-semibold'>Alam's Potflio</div>
+                <div className='text-slate-300 text-2xl font-semibold'>Alam&apos;s Potflio</div>
                 <ul className=' header rounded-xl py-2 px-5  md:flex items-center  md:gap-10 lg:gap-16 xl:gap-20 text-slate-400'>
                     <li>
                         <Link href='#about'>About Us</Link>
@@ -73,7 +73,7 @@ const Navbar = () => {
 
             {/* Navbar for small devices */}
             <div className='header px-5 lg:px-16 md:hidden flex justify-between w-screen h-20 items-center'>
-                <div className='text-slate-300 text-2xl font-semibold'>Alam's Potflio</div>
+                <div className='text-slate-300 text-2xl font-semibold'>Alam&apos;s Potflio</div>
 
                 <div className="md:hidden dropdown relative">
                     <div className="menu border rounded-md " >
--- components/about/About.tsx
+++ components/about/About.tsx
@@ -14,7 +14,7 @@ const About = () => {
 
         <p>
           <span className="absolute font-bold w-full text-center -top-4 text-2xl  pb-4 .cont text-custom-accent-200" style={{ fontFamily: 'fantasy' }}>About Me</span><br />
-          I'm Mehboob Alam, a passionate software engineering student at the University of Engineering and Technology (UET) Lahore. I'm currently pursuing my degree and gaining hands-on experience in software development, with a focus on MERN Stack technologies.
+          I&abos;m Mehboob Alam, a passionate software engineering student at the University of Engineering and Technology (UET) Lahore. I&abos;m currently pursuing my degree and gaining hands-on experience in software development, with a focus on MERN Stack technologies.
           <br />
           <span className="cont">Expertise</span>
           <br />
@@ -26,7 +26,7 @@ const About = () => {
           <br />
           <span className="cont">Learning and Growing</span>
           <br />
-          As a student, I'm constantly learning and expanding my skill set. I'm excited to apply my knowledge and skills to real-world projects and collaborate with others to build innovative solutions.
+          As a student, I&abos;m constantly learning and expanding my skill set. I&abos;m excited to apply my knowledge and skills to real-world projects and collaborate with others to build innovative solutions.
 
           Feel free to modify it as needed to fit your style and branding. Good luck with your studies and your portfolio website!</p>
       </div>
